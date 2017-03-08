677e4f2878faf61e2ebe35aa5a0dbc8f3daf6dbf
Aldiom <alex.diomedi@hotmail.com>
2017-03-08 01:16:41 -0300

    advanced in blob segmentation code

--- node_atlys/hdl_sources/blob_analyzer.v
+++ node_atlys/hdl_sources/blob_analyzer.v
@@ -22,6 +22,9 @@ module blob_analyzer(
 	localparam H_IMG_RES = 640;
 	localparam V_IMG_RES = 480;
 	localparam WIN_SIZE  = 5;
+	localparam MAX_OBJ_NUM = 15;
+	localparam B_BITS = ceil_log2(MAX_OBJ_NUM);
+	localparam [B_BITS-1:0] MAX_OBJS = MAX_OBJ_NUM;
 	
 	// ---------- MODULE ----------
 	`include "verilog_utils.vh"
@@ -51,20 +54,74 @@ module blob_analyzer(
 	);
 	
 	// Segmentation ---------------------
-	reg [3:0] line_0 [0:H_IMG_RES-1];
-	reg [3:0] line_1 [0:H_IMG_RES-1];
-	reg [11:0] prev_line = 0;
-	reg [3:0] prev_pix = 0;
+	reg [B_BITS-1:0] line_0 [0:H_IMG_RES-1];
+	reg [B_BITS-1:0] line_1 [0:H_IMG_RES-1];
+	reg [3*B_BITS-1:0] prev_line = 0;
+	reg [B_BITS-1:0] prev_pix = 0;
+	reg [B_BITS-1:0] blob_count = 0;
 	reg curr_line = 0;
-	/*
-	always @(posedge app_clk) begin
-		if(curr_line) begin
-			line_1[hpos] <= {3'b0, proc_fg_px};
+	
+	wire [MAX_OBJ_NUM-1:0] match_mask;
+	wire [B_BITS-1:0] match_count;
+	reg  [B_BITS-1:0] match_tag_hi;
+	reg  [B_BITS-1:0] match_tag_lo;
+	
+	generate
+		genvar i;
+		for(i=1; i<=MAX_OBJ_NUM; i=i+1) begin: match_block
+			assign match_mask[i-1] = (prev_line[0+:B_BITS] == i) || (prev_line[B_BITS+:B_BITS] == i)
+									|| (prev_line[2*B_BITS+:B_BITS] == i) || (prev_pix == i);
+			if(i > 2)
+				wire [B_BITS-1:0] partial_count = match_block[i-1].partial_count + match_mask[i-1];
+			else if(i == 2)
+				wire [B_BITS-1:0] partial_count = match_mask[i-2] + match_mask[i-1];
 		end
-		else begin
-			line_0[hpos] <= {3'b0, proc_fg_px};
+	endgenerate
+	
+	assign match_count = match_block[MAX_OBJ_NUM].partial_count;
+	
+	always @(*) begin
+		match_tag_hi = 0;
+		match_tag_lo = 0;
+		//we have 2 priority encoders
+		for(integer i=1; i<=MAX_OBJ_NUM; i=i+1) begin
+			if(match_mask[i-1])
+				matched_tag_hi = i;
+		end
+		for(integer i=MAX_OBJ_NUM; i>=1; i=i-1) begin
+			if(match_mask[i-1])
+				matched_tag_lo = i;
+		end
 	end
-	*/
+	
+	always @(posedge app_clk) begin
+		if(proc_fg_px) begin
+			if(match_count == 0) begin //then it's a new blob
+				blob_count <= (blob_count < MAX_OBJS) ? blob_count + 1 : MAX_OBJS;
+				prev_pix <= blob_count + 1;
+				if(curr_line)
+					line_1[hpos] <= (blob_count < MAX_OBJS) ? blob_count + 1 : 0;
+				else //check if last line... or first line?
+					line_0[hpos] <= (blob_count < MAX_OBJS) ? blob_count + 1 : 0;;
+			end
+			else if(match_count == 1) begin //then it's a known blob
+				prev_pix <= match_tag_lo;
+				//and do stuff to get borders
+				if(curr_line) 
+					line_1[hpos] <= match_tag_lo;
+				else
+					line_0[hpos] <= match_tag_lo;				
+			end
+			else //then we have a blob collision
+				prev_pix <= match_tag_hi;
+				//and do stuff
+				if(curr_line)
+					line_1[hpos] <= match_tag_hi;
+				else
+					line_0[hpos] <= match_tag_hi;
+		end
+	end
+
 	// Read buffer with video clock
 	wire [18:0] wr_addr = vid_hpos + H_IMG_RES*vpos_off2;
 	wire [18:0] rd_addr = vid_hpos + H_IMG_RES*vid_vpos;
